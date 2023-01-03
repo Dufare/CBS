07e5ed0a7075601fb320a7cf59e1b6f0fab9269b
jayesh.dufare <jdufare04@gmail.com>
2023-01-03 11:39:58 +0530

    project start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+    Hiii
+    <navbar/>
+    </div>
+  );
+}
+
+export default App;
